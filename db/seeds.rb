26ba2575428d057c01fcc0f77dcfecb8f30dc5e0
amninder singh <45424062+amnindersingh12@users.noreply.github.com>
2022-10-27 10:06:39 +0530

    Add Expense User Category Model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Seeding users'
+User.create(email:"user1@gmail.com", first_name:"user1",last_name:"expenso",password:"11111111",username:"user1")
+User.create(email:"user2@gmail.com", first_name:"user2",last_name:"expenso",password:"11111111",username:"user2")
+User.create(email:"user3@gmail.com", first_name:"user3",last_name:"expenso",password:"11111111",username:"user3")
+User.create(email:"user4@gmail.com", first_name:"user4",last_name:"expenso",password:"11111111",username:"user4")
+puts 'Seeding done'
