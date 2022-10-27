86a4e092e2d43aa5a093a52fd59c5603925f3dbc
amninder singh <45424062+amnindersingh12@users.noreply.github.com>
2022-10-27 13:14:13 +0530

    User can add expense and delete expenses

--- db/migrate/20221027040853_create_expenses.rb
+++ db/migrate/20221027040853_create_expenses.rb
@@ -1,10 +1,10 @@
 class CreateExpenses < ActiveRecord::Migration[7.0]
   def change
     create_table :expenses do |t|
-      t.references :category, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
+      t.integer :user_id, null: false
+      t.integer :category_id, null: false
       t.float :amount, null: false
-      t.string :date
+      t.datetime :date
 
       t.timestamps
     end
